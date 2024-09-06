1b759df2b111263cb4a4ef101c1bf333ce7100af
Kirill Griskun <kirill.griskun@gmail.com>
2024-09-06 15:00:46 +0300

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.BackgroundColor = ConsoleColor.Red;
+Console.WriteLine("Hello, World!");
+
+Console.BackgroundColor = ConsoleColor.Black;
+Console.WriteLine("Enter your name:"); //Вывод
+
+/*
+string userName;
+userName = Console.ReadLine();
+*/
+
+string userName = Console.ReadLine(); //Ввод
+
+//Console.WriteLine("Hello" + ", " + userName + "!");
+Console.WriteLine($"Hello, {userName}!");
